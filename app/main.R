22831a67d4aa84bfbccb3ea1277f2041a1b50b40
Kamil Zyla <kamil@appsilon.com>
2022-05-31 12:30:02 +0200

    feat: Route submodules

--- app/main.R
+++ app/main.R
@@ -1,5 +1,6 @@
 box::use(
-  shiny,
+  shiny, shiny[tags],
+  shiny.router[make_router, route, route_link],
   shiny.stats,
 )
 box::use(
@@ -7,22 +8,27 @@ box::use(
   app/view[map, modal],
 )
 
-#' @export
-ui <- function(id) {
-  ns <- shiny$NS(id)
-  shiny$bootstrapPage(
-    map$ui(ns("map")),
-    modal$ui(ns("modal"))
-  )
-}
+menu <- tags$ul(
+  tags$li(tags$a(href = route_link("/"), "Map")),
+  tags$li(tags$a(href = route_link("modal"), "Modal"))
+)
+
+router <- make_router(
+  route("/", map$ui("map"), function() map$server("map")),
+  route("modal", modal$ui("modal"), function() modal$server("modal"))
+)
 
 #' @export
-server <- function(id) {
-  shiny$moduleServer(id, function(input, output, session) {
-    map$server("map")
-    modal$server("modal")
-    stats_connection <- stats$connect()
-    shiny.stats$log_login(stats_connection)
-    shiny.stats$log_logout(stats_connection)
-  })
+ui <- shiny$bootstrapPage(
+  menu,
+  router$ui
+)
+
+#' @export
+server <- function(input, output, session) {
+  router$server(input, output, session)
+
+  stats_connection <- stats$connect()
+  shiny.stats$log_login(stats_connection)
+  shiny.stats$log_logout(stats_connection)
 }
