a97ea0c111d8e3481b60dfcc14508ea44bde83b6
deepanshkhurana <deepanshkhurana@outlook.com>
2023-02-16 13:38:54 +0530

    feat: update `scripts/get_countries_map_data.R` for new data source

--- scripts/get_countries_map_data.R
+++ scripts/get_countries_map_data.R
@@ -1,34 +1,37 @@
-map_url <- paste0(
-  "http://www.naturalearthdata.com/http//www.naturalearthdata.com/",
-  "download/50m/cultural/ne_50m_admin_0_countries.zip"
-)
+# Note: if the map_url below fails to work, you can also get the dataset
+# manually from the link below:
+# https://hub.arcgis.com/datasets/esri::world-countries-generalized/explore?location=-0.062553%2C0.000000%2C2.00
 
-file <- paste0("./scripts/raw_data/", basename(map_url))
+map_url <-
+  "https://opendata.arcgis.com/api/v3/datasets/2b93b06dc0dc4e809d3c8db5cb96ba69_0/downloads/data?format=shp&spatialRefId=4326&where=1%3D1"
+
+file <- paste0("./scripts/raw_data/", "world_map_data", ".zip")
 
 if (!file.exists(file)) download.file(map_url, file)
 
 tmp <- tempdir()
 unzip(file, exdir = tmp)
 
-countries <- st_read(
-  dsn = tmp,
-  layer = strsplit(basename(map_url), split = "\\.")[[1]][1],
-  stringsAsFactors = FALSE
+countries <- sf::st_read(
+  dsn = tmp
 )
 
-data <- read_fst(path = "./app/data/olympics_data.fst")
+data <- fst::read_fst(path = "./app/data/olympics_data.fst")
+
+countries <- countries |>
+  dplyr::mutate(ISO3c = countrycode::countrycode(
+    ISO,
+    origin = "iso2c",
+    destination = "iso3c"
+  ))
 
 map_data <-
-  countries %>%
-  mutate(
-    ISO3c = case_when(
-      ISO_A3 == "-99" ~ ADM0_A3, # replace missing codes
-      TRUE ~ ISO_A3
-    ),
-    centroid_LON = st_coordinates(st_centroid(geometry))[, 1],
-    centroid_LAT = st_coordinates(st_centroid(geometry))[, 2]
-  ) %>%
-  dplyr::select(c(NAME_LONG, ISO3c, geometry, centroid_LON, centroid_LAT)) %>%
+  countries |>
+  dplyr::mutate(
+    centroid_LON = sf::st_coordinates(sf::st_centroid(geometry))[, 1],
+    centroid_LAT = sf::st_coordinates(sf::st_centroid(geometry))[, 2]
+  ) |>
+  dplyr::select(c(COUNTRY, ISO3c, geometry, centroid_LON, centroid_LAT)) |>
   dplyr::filter(ISO3c %in% unique(data$ISO3c))
 
-st_write(map_data, dsn = "./app/data/map_data.shp")
+sf::st_write(map_data, dsn = "./app/data/map_data.shp")
